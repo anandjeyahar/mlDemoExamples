2cca175d5e5177d3159bf4b9b9daedcccdd136a9
anand <anand.jeyahar@gmail.com>
2016-12-14 22:57:43 +0530

    Minor formatting

--- tpot_iris_pipeline.py
+++ tpot_iris_pipeline.py
@@ -8,7 +8,9 @@ from sklearn.preprocessing import FunctionTransformer, Normalizer
 
 # NOTE: Make sure that the class is labeled 'class' in the data file
 tpot_data = np.recfromcsv('./data/Iris.csv', delimiter='COLUMN_SEPARATOR', dtype=np.float64)
-features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1), tpot_data.dtype.names.index('class'), axis=1)
+features = np.delete(tpot_data.view(np.float64).reshape(tpot_data.size, -1),
+                     tpot_data.dtype.names.index('class'),
+                     axis=1)
 training_features, testing_features, training_classes, testing_classes = \
     train_test_split(features, tpot_data['class'], random_state=42)
 
